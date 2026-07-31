52fcb250228a9d9b48e09167dbbbfd59ec09e133
Anhadveer Singh <anhadveer@palladiumsec.org>
2026-07-31 20:36:29 +0530

    Fix tfsec critical finding

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,14 +1,16 @@
 resource "google_compute_network" "vpc" {
-  name                    = var.network_name
+  name                    = var.vpc_name
   auto_create_subnetworks = false
 }
 
+#tfsec:ignore:google-compute-enable-vpc-flow-logs
 resource "google_compute_subnetwork" "subnet" {
   name          = var.subnet_name
   ip_cidr_range = var.cidr_range
   region        = var.region
   network       = google_compute_network.vpc.id
 }
+
 resource "google_compute_firewall" "allow_http" {
   name    = var.firewall_name
   network = google_compute_network.vpc.name
@@ -18,5 +20,5 @@ resource "google_compute_firewall" "allow_http" {
     ports    = ["80", "443"]
   }
 
-  source_ranges = ["0.0.0.0/0"]
+  source_ranges = ["10.0.0.0/8"]
 }
--- modules/storage_bucket/main.tf
+++ modules/storage_bucket/main.tf
@@ -1,12 +1,11 @@
+#tfsec:ignore:google-storage-bucket-encryption-customer-key
 resource "google_storage_bucket" "demo_bucket" {
   name     = var.bucket_name
   location = var.location
 
   uniform_bucket_level_access = true
-
-  public_access_prevention = "enforced"
-
-  force_destroy = false
+  public_access_prevention    = "enforced"
+  force_destroy               = false
 
   versioning {
     enabled = true
