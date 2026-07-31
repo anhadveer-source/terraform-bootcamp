5a394b1d085c2560d43be8d21687633f39e7b7c5
Anhadveer Singh <anhadveer@palladiumsec.org>
2026-07-31 20:18:48 +0530

    Add tfsec security scan

--- modules/storage_bucket/main.tf
+++ modules/storage_bucket/main.tf
@@ -1,5 +1,10 @@
 resource "google_storage_bucket" "demo_bucket" {
-  name                        = var.bucket_name
-  location                    = var.location
-  uniform_bucket_level_access = true
+  name     = var.bucket_name
+  location = var.location
+
+  uniform_bucket_level_access = false
+
+  public_access_prevention = "inherited"
+
+  force_destroy = true
 }
